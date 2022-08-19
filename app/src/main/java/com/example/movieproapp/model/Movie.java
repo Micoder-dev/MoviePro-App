6edc0158f013c96113395a50aed376f1415a3d3f
Micoder-dev <micoder.com@gmail.com>
2022-08-19 22:42:25 +0530

    Project Setup and API overview "api key generated"

---
+++ app/src/main/java/com/example/movieproapp/model/Movie.java
@@ -0,0 +1,11 @@
+package com.example.movieproapp.model;
+
+import android.os.Parcelable;
+
+import androidx.databinding.BaseObservable;
+
+public class Movie extends BaseObservable implements Parcelable {
+
+
+
+}
