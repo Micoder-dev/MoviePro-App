1a56850d9ce678005843a29da040fe2a15421c9f
Micoder-dev <micoder.com@gmail.com>
2022-08-20 10:43:58 +0530

    Retrofit Calls

--- app/src/main/java/com/example/movieproapp/model/MovieRepository.java
+++ app/src/main/java/com/example/movieproapp/model/MovieRepository.java
@@ -4,10 +4,16 @@ import android.app.Application;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.movieproapp.R;
 import com.example.movieproapp.service.MovieDataService;
+import com.example.movieproapp.service.RetrofitInstance;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MovieRepository {
 
@@ -21,8 +27,28 @@ public class MovieRepository {
 
     public MutableLiveData<List<Movie>> getMutableLiveData() {
 
-        MovieDataService movieDataService = RetrofitInstance.
+        MovieDataService movieDataService = RetrofitInstance.getService();
 
+        Call<Result> call = movieDataService.getPopularMovies(application.getApplicationContext().getString(R.string.api_key));
+
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                Result result = response.body();
+
+                if (result != null && result.getResults() != null) {
+                    movies = (ArrayList<Movie>) result.getResults();
+                    mutableLiveData.setValue(movies);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+
+            }
+        });
+
+        return mutableLiveData;
     }
 
 }
