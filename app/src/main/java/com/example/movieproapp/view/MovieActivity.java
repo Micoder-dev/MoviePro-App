b7e8e3be5087f78a2c7b762038b3bde11c9b2b4d
Micoder-dev <micoder.com@gmail.com>
2022-08-20 14:18:21 +0530

    Movie Details Activity

--- app/src/main/java/com/example/movieproapp/view/MovieActivity.java
+++ app/src/main/java/com/example/movieproapp/view/MovieActivity.java
@@ -1,16 +1,41 @@
 package com.example.movieproapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.movieproapp.R;
+import com.example.movieproapp.databinding.ActivityMovieBinding;
+import com.example.movieproapp.model.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private Movie movie;
+    private ActivityMovieBinding activityMovieBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        activityMovieBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie);
+
+        Intent i = getIntent();
+
+        if (i != null) {
+            movie = i.getParcelableExtra("movie");
+            activityMovieBinding.setMovie(movie);
+            getSupportActionBar().setTitle(movie.getTitle());
+
+        }
     }
 }
