70f4456b661adf412fead2700af8ebc3baa62b11
Micoder-dev <micoder.com@gmail.com>
2022-08-20 10:33:56 +0530

    Retrofit Instance

---
+++ app/src/main/java/com/example/movieproapp/service/RetrofitInstance.java
@@ -0,0 +1,25 @@
+package com.example.movieproapp.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static MovieDataService getService() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(MovieDataService.class);
+
+    }
+
+
+}
