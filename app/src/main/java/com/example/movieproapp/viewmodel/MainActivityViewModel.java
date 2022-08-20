9d62a0e9b0aa688d3fe6980b73f0de8eab6eaba2
Micoder-dev <micoder.com@gmail.com>
2022-08-20 10:56:29 +0530

    View Model

---
+++ app/src/main/java/com/example/movieproapp/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,29 @@
+package com.example.movieproapp.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.movieproapp.model.Movie;
+import com.example.movieproapp.model.MovieRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private MovieRepository movieRepository;
+
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        movieRepository = new MovieRepository(application);
+    }
+
+    // Live Data
+    public LiveData<List<Movie>> getAllMovies() {
+        return movieRepository.getMutableLiveData();
+    }
+
+}
